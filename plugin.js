26602ceba41ea084a4dae90d4d6adbe03f1cf05e
Mikael Jenkler <mikael@jenkler.com>
2023-07-31 14:53:04 +0200

    Fixed admin route to admin/plugins/rss-widget, so it doesn't conflict with nodebb-plugin-rss and matches other plugin pages Renamed admin.js to public/admin.js Updated compatibility to Nodebb 3.2.0 Updated LICENSE Updated templates to use Bootstrap 5 classes

--- plugin.js
+++ plugin.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const helpers = require.main.require('./src/routes/helpers');
 const meta = require.main.require('./src/meta');
 const rssparser = new (require('rss-parser'));
 let nodebb = {};
@@ -39,7 +40,9 @@ const updateFeed = async (force = false) => {
   else { return false }
 }
 const renderAdmin = async (req, res) => {
-  res.render('admin/rss', {});
+  res.render('admin/plugins/rss-widget', {
+    title: 'RSS Widget',
+  });
 }
 const rsort = async (data) => {
   return Object.keys(data).sort().reverse().reduce((r, k) => (r[k] = data[k], r), {});
@@ -48,8 +51,8 @@ const rsort = async (data) => {
 exports.filterAdminHeaderBuild = async (data) => {
   data.plugins.push({
     icon: 'fa-link',
-    name: 'RSS',
-    route: '/rss'
+    name: 'RSS Widget',
+    route: '/plugins/rss-widget'
   });
   return data;
 };
@@ -75,8 +78,7 @@ exports.filterWidgetsGetWidgets = async (data) => {
 };
 exports.staticAppLoad = async (data) => {
   console.log('Loading Jenkler RSS widget ' + require('./package.json').version);
-  data.router.get('/admin/rss', data.middleware.admin.buildHeader, renderAdmin);
-  data.router.get('/api/admin/rss', renderAdmin);
+  helpers.setupAdminPageRoute(data.router, '/admin/plugins/rss-widget', renderAdmin);
   nodebb.app = data.app;
   await updateFeed(true);
 };
---
+++ templates/admin/plugins/rss-widget.tpl
@@ -0,0 +1,18 @@
+<div class="acp-page-container">
+  <!-- IMPORT admin/partials/settings/header.tpl -->
+  <div class="row m-0">
+    <div id="spy-container" class="col-12 px-0 mb-4" tabindex="0">
+      <form class="rss-settings" role="form">
+        <p class="lead">A widget that shows your rss feeds</p>
+        <div class="mb-3">
+          <label class="form-label" for="urls">Urls</label>
+          <input class="form-control" id="urls" name="urls" placeholder="Comma separated list of urls to RSS feeds." title="Urls" type="text">
+        </div>
+        <div class="">
+          <label class="form-label" for="limit">limit</label>
+          <input class="form-control" id="limit" name="limit" placeholder="Number entries form each feed url. Default is 10." title="Limit" type="text">
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
